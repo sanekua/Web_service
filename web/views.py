5db45a1d513dcc6dfde836b3e89aab93d2058baf
sanekua <sasha-kuzmyk@mail.ru>
2019-05-23 21:42:19 +0300

    third step

--- web/views.py
+++ web/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+import time
+from .models import Post
 
-# Create your views here.
+
+
+# def home(request):
+#     return render(request,'web/home.html')
+#
+# def about(request):
+#     return render(request, 'web/about.html')
+
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'web/home.html', context)
+
+
+def about(request):
+    return render(request, 'web/about.html', {'title': 'About'})
+
