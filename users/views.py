4ec9e0817bfeb210b1ee343798bd88ba257b9d8d
sanekua <sasha-kuzmyk@mail.ru>
2019-05-24 03:29:25 +0300

    6 step

--- users/views.py
+++ users/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, User
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 import time
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+            return redirect('web-home')
+    else:
+        form = UserRegisterForm()
+
     return render(request,'users/register.html',{'form':form})
 
 
